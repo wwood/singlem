965d32754a991a4d100205214867fc529c472bce
Ben J Woodcroft <donttrustben@gmail.com>
2016-05-23 08:53:52 +1000

    package_creator: beautify wrapping

--- singlem/package_creator.py
+++ singlem/package_creator.py
@@ -25,26 +25,31 @@ class PackageCreator:
         gpkg = GraftMPackage.acquire(input_graftm_package_path)
         tree_leaves = set()
         for node in TreeNode.read(gpkg.reference_package_tree_path()).tips():
-            # need to replace here because otherwise they don't line up with the diamond database IDs
+            # need to replace here because otherwise they don't line up with the
+            # diamond database IDs
             node_name = node.name.replace(' ','_')
             if node_name in tree_leaves:
-                raise Exception("Found duplicate tree leaf name in graftm package tree. Currently this case is not handled, sorry")
+                raise Exception("Found duplicate tree leaf name in graftm package "
+                                "tree. Currently this case is not handled, sorry")
             tree_leaves.add(node_name)
         for name in tree_leaves: #I don't think there is a 'peek' ?
             eg_name = name
             break
-        logging.info("Read in %i tree tip names e.g. %s" % (len(tree_leaves),
-                                                            eg_name))
+        logging.info("Read in %i tree tip names e.g. %s" % (
+            len(tree_leaves), eg_name))
         
         # Make a new fasta file of all the sequences that are leaves
         found_sequence_names = set()
         num_seqs_unaligned = 0
-        with tempfile.NamedTemporaryFile(prefix='singlem_package_creator',suffix='.fasta') as t:
+        with tempfile.NamedTemporaryFile(prefix='singlem_package_creator',
+                                         suffix='.fasta') as t:
             for s in SeqIO.parse(gpkg.unaligned_sequence_database_path(), "fasta"):
                 num_seqs_unaligned += 1
                 if s.id in tree_leaves:
                     if s.id in found_sequence_names:
-                        raise Exception("Found duplicate sequence names in graftm unaligned sequence fasta file. Currently this case is not handled, sorry")
+                        raise Exception("Found duplicate sequence names in graftm unaligned"
+                                        " sequence fasta file. Currently this case is not handled,"
+                                        " sorry")
                     SeqIO.write([s], t, "fasta")
                     found_sequence_names.add(s.id)
             t.flush()
@@ -52,18 +57,23 @@ class PackageCreator:
             if len(tree_leaves) != len(found_sequence_names):
                 for t in tree_leaves:
                     if t not in found_sequence_names:
-                        raise Exception("Found some sequences that were in the tree but not the unaligned sequences database e.g. %s. Something is likely amiss with the input GraftM package" % t)
+                        raise Exception("Found some sequences that were in the tree but not the"
+                                        " unaligned sequences database e.g. %s. Something is likely"
+                                        " amiss with the input GraftM package" % t)
                 raise Exception("Programming error, shouldn't get here")
-            logging.info("All %i sequences found in tree extracted successfully from unaligned sequences fasta file, which originally had %i sequences" % (\
-                len(found_sequence_names), num_seqs_unaligned))
+            logging.info("All %i sequences found in tree extracted successfully from unaligned"
+                         " sequences fasta file, which originally had %i sequences" % (
+                             len(found_sequence_names), num_seqs_unaligned))
             
             # Create a new diamond database
+            
             with tempfile.NamedTemporaryFile(prefix='singlem_package_creator',suffix='.dmnd') as dmnd:
                 cmd = "diamond makedb --in '%s' -d '%s'" % (t.name, dmnd.name)
                 extern.run(cmd)
             
                 # Compile the final graftm/singlem package
-                if len(gpkg.search_hmm_paths()) == 1 and gpkg.search_hmm_paths()[0] == gpkg.alignment_hmm_path():
+                if len(gpkg.search_hmm_paths()) == 1 and \
+                   gpkg.search_hmm_paths()[0] == gpkg.alignment_hmm_path():
                     search_hmms = None
                 else:
                     search_hmms = gpkg.search_hmm_paths()
@@ -83,7 +93,8 @@ class PackageCreator:
                                                   search_hmms)
                     logging.debug("Finished creating GraftM package for conversion to SingleM package")
                 
-                    SingleMPackageVersion1.compile(output_singlem_package_path, gpkg_name, hmm_position)
+                    SingleMPackageVersion1.compile(output_singlem_package_path,
+                                                   gpkg_name, hmm_position)
                     
                     shutil.rmtree(gpkg_name)
                     
