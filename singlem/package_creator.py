92c931b3e08d664f866f484debb2aab28485897d
Ben Woodcroft <donttrustben@gmail.com>
2016-06-19 09:38:24 +1000

    package_creator: fix nucleotide pkg creation, add tests

--- singlem/package_creator.py
+++ singlem/package_creator.py
@@ -49,64 +49,66 @@ class PackageCreator:
             # Make a new fasta file of all the sequences that are leaves
             found_sequence_names = set()
             num_seqs_unaligned = 0
-            with tempfile.NamedTemporaryFile(prefix='singlem_package_creator',
-                                             suffix='.fasta') as t:
-                for s in SeqIO.parse(gpkg.unaligned_sequence_database_path(), "fasta"):
-                    num_seqs_unaligned += 1
-                    if s.id in tree_leaves:
-                        if s.id in found_sequence_names:
-                            raise Exception("Found duplicate sequence names in graftm unaligned"
-                                            " sequence fasta file. Currently this case is not handled,"
-                                            " sorry")
-                        SeqIO.write([s], t, "fasta")
-                        found_sequence_names.add(s.id)
-                t.flush()
+            filtered_aligned_tempfile = tempfile.NamedTemporaryFile(prefix='singlem_package_creator',
+                                                                    suffix='.fasta')
+            for s in SeqIO.parse(gpkg.unaligned_sequence_database_path(), "fasta"):
+                num_seqs_unaligned += 1
+                if s.id in tree_leaves:
+                    if s.id in found_sequence_names:
+                        raise Exception("Found duplicate sequence names in graftm unaligned"
+                                        " sequence fasta file. Currently this case is not handled,"
+                                        " sorry")
+                    SeqIO.write([s], filtered_aligned_tempfile, "fasta")
+                    found_sequence_names.add(s.id)
+            filtered_aligned_tempfile.flush()
 
-                if len(tree_leaves) != len(found_sequence_names):
-                    for t in tree_leaves:
-                        if t not in found_sequence_names:
-                            raise Exception("Found some sequences that were in the tree but not the"
-                                            " unaligned sequences database e.g. %s. Something is"
-                                            " likely amiss with the input GraftM package" % t)
-                    raise Exception("Programming error, shouldn't get here")
-                logging.info("All %i sequences found in tree extracted successfully from unaligned"
-                             " sequences fasta file, which originally had %i sequences" % (
-                                 len(found_sequence_names), num_seqs_unaligned))
+            if len(tree_leaves) != len(found_sequence_names):
+                for t in tree_leaves:
+                    if t not in found_sequence_names:
+                        raise Exception("Found some sequences that were in the tree but not the"
+                                        " unaligned sequences database e.g. %s. Something is"
+                                        " likely amiss with the input GraftM package" % t)
+                raise Exception("Programming error, shouldn't get here")
+            logging.info("All %i sequences found in tree extracted successfully from unaligned"
+                         " sequences fasta file, which originally had %i sequences" % (
+                             len(found_sequence_names), num_seqs_unaligned))
 
-                # Create a new diamond database
-                dmnd_tf = tempfile.NamedTemporaryFile(prefix='singlem_package_creator',suffix='.dmnd')
-                cmd = "diamond makedb --in '%s' -d '%s'" % (t.name, dmnd_tf.name)
-                logging.info("Creating DIAMOND database")
-                extern.run(cmd)
+            # Create a new diamond database
+            dmnd_tf = tempfile.NamedTemporaryFile(prefix='singlem_package_creator',suffix='.dmnd')
+            cmd = "diamond makedb --in '%s' -d '%s'" % (filtered_aligned_tempfile.name, dmnd_tf.name)
+            logging.info("Creating DIAMOND database")
+            extern.run(cmd)
                 
-                # Compile the final graftm/singlem package
-                if len(gpkg.search_hmm_paths()) == 1 and \
-                   gpkg.search_hmm_paths()[0] == gpkg.alignment_hmm_path():
-                    search_hmms = None
-                else:
-                    search_hmms = gpkg.search_hmm_paths()
+        # Compile the final graftm/singlem package
+        if len(gpkg.search_hmm_paths()) == 1 and \
+           gpkg.search_hmm_paths()[0] == gpkg.alignment_hmm_path():
+            search_hmms = None
+        else:
+            search_hmms = gpkg.search_hmm_paths()
 
-                with tempdir.TempDir() as tmpdir:
-                    gpkg_name = os.path.join(
-                        tmpdir,
-                        os.path.basename(
-                            os.path.abspath(input_graftm_package_path)).replace('.gpkg',''))
-                    GraftMPackageVersion3.compile(gpkg_name,
-                                                  gpkg.reference_package_path(),
-                                                  gpkg.alignment_hmm_path(),
-                                                  dmnd_tf.name if is_protein_package else None,
-                                                  gpkg.maximum_range(),
-                                                  t.name if is_protein_package else \
-                                                      gpkg.unaligned_sequence_database_path(),
-                                                  gpkg.use_hmm_trusted_cutoff(),
-                                                  search_hmms)
-                    logging.debug("Finished creating GraftM package for conversion to SingleM package")
+        with tempdir.TempDir() as tmpdir:
+            gpkg_name = os.path.join(
+                tmpdir,
+                os.path.basename(
+                    os.path.abspath(input_graftm_package_path)).replace('.gpkg',''))
+            GraftMPackageVersion3.compile(gpkg_name,
+                                          gpkg.reference_package_path(),
+                                          gpkg.alignment_hmm_path(),
+                                          dmnd_tf.name if is_protein_package else None,
+                                          gpkg.maximum_range(),
+                                          filtered_aligned_tempfile.name if is_protein_package else \
+                                              gpkg.unaligned_sequence_database_path(),
+                                          gpkg.use_hmm_trusted_cutoff(),
+                                          search_hmms)
+            logging.debug("Finished creating GraftM package for conversion to SingleM package")
 
-                    SingleMPackageVersion1.compile(output_singlem_package_path,
-                                                   gpkg_name, hmm_position)
+            SingleMPackageVersion1.compile(output_singlem_package_path,
+                                           gpkg_name, hmm_position)
 
-                    shutil.rmtree(gpkg_name)
-                    if is_protein_package: dmnd_tf.close()
+            shutil.rmtree(gpkg_name)
+            if is_protein_package:
+                filtered_aligned_tempfile.close()
+                dmnd_tf.close()
 
-                    logging.info("SingleM-compatible package creation finished")
+            logging.info("SingleM-compatible package creation finished")
 
---
+++ test/test_package_creator.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+#=======================================================================
+# Authors: Ben Woodcroft
+#
+# Unit tests.
+#
+# Copyright
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License.
+# If not, see <http://www.gnu.org/licenses/>.
+#=======================================================================
+
+import unittest
+import os.path
+from string import split
+import sys
+from StringIO import StringIO
+import tempdir
+import json
+
+path_to_script = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..','bin','singlem')
+path_to_data = os.path.join(os.path.dirname(os.path.realpath(__file__)),'data')
+
+sys.path = [os.path.join(os.path.dirname(os.path.realpath(__file__)),'..')]+sys.path
+from singlem.package_creator import PackageCreator
+
+class Tests(unittest.TestCase):
+    maxDiff = None
+
+    def test_create_protein_pkg(self):
+        with tempdir.in_tempdir():
+            PackageCreator().create(
+                input_graftm_package = os.path.join(
+                    path_to_data, '4.11.22seqs.gpkg.spkg', '4.11.22seqs'),
+                output_singlem_package = 'protein.spkg',
+                hmm_position = 76,
+                force = False)
+            self.assertTrue(os.path.isdir('protein.spkg'))
+            j = json.load(open('protein.spkg/CONTENTS.json'))
+            self.assertEqual(76, j['singlem_hmm_position'])
+
+    def test_create_nuc_pkg(self):
+        with tempdir.in_tempdir():
+            PackageCreator().create(
+                input_graftm_package = os.path.join(
+                    path_to_data, '61_otus.v3.gpkg.spkg', '61_otus.v3'),
+                output_singlem_package = 'nuc.spkg',
+                hmm_position = 888,
+                force = False)
+            self.assertTrue(os.path.isdir('nuc.spkg'))
+            j = json.load(open('nuc.spkg/CONTENTS.json'))
+            self.assertEqual(888, j['singlem_hmm_position'])
+            
+       
+
+if __name__ == "__main__":
+    unittest.main()
