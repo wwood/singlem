b6dde66c9db1b7fce71eadbba2cdd194d508ba98
Ben Woodcroft <donttrustben@gmail.com>
2016-10-21 22:03:13 +1000

    single: summariser: Remove extraneous whitespace.

--- singlem/summariser.py
+++ singlem/summariser.py
@@ -10,13 +10,13 @@ class Summariser:
     def summarise(**kwargs):
         '''Summarise an OTU table'''
         krona_output_prefix = kwargs.pop('krona_output_prefix')
-        table_collection = kwargs.pop('table_collection')        
+        table_collection = kwargs.pop('table_collection')
         if len(kwargs) > 0:
             raise Exception("Unexpected arguments detected: %s" % kwargs)
-        
+
         # prep the array
         gene_to_sample_to_taxonomy_to_count = Summariser._collapse_otu_table_into_gene_to_sample_to_taxonomy_to_count(table_collection)
-            
+
         # write the input krona files
         sample_name_to_tempfile = OrderedDict()
         for gene, sample_to_taxonomy_to_count in gene_to_sample_to_taxonomy_to_count.iteritems():
@@ -26,7 +26,7 @@ class Summariser:
             for sample, taxonomy_to_count in sample_to_taxonomy_to_count.iteritems():
                 f = tempfile.NamedTemporaryFile(prefix='singlem_for_krona')
                 sample_name_to_tempfile[sample] = f
-                
+
                 for taxonomy, coverage in taxonomy_to_count.iteritems():
                     tax_split = taxonomy.split('; ')
                     if tax_split[0] == 'Root' and len(tax_split) > 1: tax_split = tax_split[1:]
@@ -37,7 +37,7 @@ class Summariser:
             extern.run(cmd)
             for f in sample_name_to_tempfile.values():
                 f.close()
-                
+
     @staticmethod
     def _collapse_otu_table_into_gene_to_sample_to_taxonomy_to_count(table_collection,
                                                                      add_sequence_to_taxonomy=True,
@@ -63,12 +63,12 @@ class Summariser:
                 record = otu.count
             gene_to_sample_to_taxonomy_to_count[otu.marker][otu.sample_name][tax] = record
         return gene_to_sample_to_taxonomy_to_count
-        
+
     @staticmethod
     def write_unifrac_format_file(**kwargs):
         '''Summarise an OTU table as 3 column tab separated OTU table:
         sample, taxonomy, count
-        
+
         When >1 OTU maps to the same taxonomy, collapse them into one OTU,
         adding their coverages
         '''
@@ -76,10 +76,10 @@ class Summariser:
         table_collection = kwargs.pop('table_collection')
         if len(kwargs) > 0:
             raise Exception("Unexpected arguments detected: %s" % kwargs)
-        
+
         # prep the array
         gene_to_sample_to_taxonomy_to_count = Summariser._collapse_otu_table_into_gene_to_sample_to_taxonomy_to_count(table_collection, add_sequence_to_taxonomy=False, use_coverage=False)
-        
+
         for gene, sample_to_taxonomy_to_count in gene_to_sample_to_taxonomy_to_count.items():
             unifrac_output = '%s.%s.unifrac' % (unifrac_output_prefix, gene)
             logging.info("Writing %s" % unifrac_output)
@@ -107,7 +107,7 @@ class Summariser:
                                    fields_to_print=table_collection.example_field_names())
         else:
             OtuTable.write_otus_to(table_collection, output_table_io)
-            
+
     @staticmethod
     def write_clustered_otu_table(**kwargs):
         output_table_io = kwargs.pop('output_table_io')
@@ -125,7 +125,7 @@ class Summariser:
                  'num_sub_otus',
                  'max_sub_otu_abundance'])
             +"\n")
-        
+
         for d in table_collection:
             for otu in d.otus:
                 output_table_io.write("\t".join(
@@ -165,12 +165,3 @@ class Summariser:
         logging.info("Rarefying OTU table to max %i sequences per sample/gene combination and writing to %s" % (number_to_choose, output_table_io.name))
         OtuTable.write_otus_to(Rarefier().rarefy(table_collection, number_to_choose),
                                output_table_io)
-                                 
-
-        
-        
-                        
-        
-        
-        
-        
