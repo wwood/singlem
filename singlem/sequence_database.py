a72ec7ac014cb86beb68a61e2c5d56caeb4b25a1
Ben Woodcroft <donttrustben@gmail.com>
2017-09-17 09:23:10 +1000

    makedb/query: Replace BLAST+ with smafa.

--- singlem/sequence_database.py
+++ singlem/sequence_database.py
@@ -6,6 +6,8 @@ import logging
 import subprocess
 import itertools
 import sqlite3
+import glob
+
 from itertools import izip_longest
 from threading import Thread
 from Bio import SeqIO
@@ -49,50 +51,36 @@ class DBSequence(OtuTableEntry):
         return to_return
 
 class SequenceDatabase:
-    version = 1
+    version = 2
     GAP_REPLACEMENT_CHARACTER = 'Y'
     DEFLINE_DELIMITER_CHARACTER = '~'
     SQLITE_DB_NAME = 'otus.sqlite3'
+    _marker_to_smafadb = {}
+
+    def add_smafa_db(self, marker_name, smafa_db):
+        self._marker_to_smafadb[marker_name] = smafa_db
+
+    def get_smafa_db(self, marker_name):
+        if marker_name in self._marker_to_smafadb:
+            return self._marker_to_smafadb[marker_name]
+        else:
+            logging.debug("No smafa DB found for %s" % marker_name)
+            return None
+
+    def smafa_dbs(self):
+        return self._marker_to_smafadb.values()
 
     @staticmethod
     def acquire(path):
         db = SequenceDatabase()
-        db.sequences_fasta_file = os.path.join(path, "sequences.fasta")
         db.sqlite_file = os.path.join(path, SequenceDatabase.SQLITE_DB_NAME)
+        smafas = glob.glob("%s/*.smafadb" % path)
+        logging.debug("Found smafadbs: %s" % ", ".join(smafas))
+        if len(smafas) == 0: raise Exception("No smafa DBs found in DB")
+        for g in smafas:
+            marker = os.path.basename(g).replace('.smafadb','') # TODO: Make a contents file.
+            db.add_smafa_db(marker, g)
         return db
-
-    @staticmethod
-    def write_dereplicated_fasta_file(input_stream, output_stream):
-        logging.debug("Starting to write FASTA file in thread")
-        last_sequence = None
-        ids_to_print = []
-        num_printed = 0
-        sequence_id = 1
-        for d in csv.reader(input_stream, delimiter="\t"):
-            if SequenceDatabase.DEFLINE_DELIMITER_CHARACTER in d[1]:
-                raise Exception(
-                    "Taxonomy and sample names cannot have '%s' in them" % \
-                    SequenceDatabase.DEFLINE_DELIMITER_CHARACTER)
-            if d[0] == last_sequence:
-                ids_to_print.append(d[1])
-            else:
-                if ids_to_print != []: # if not the first line
-                    output_stream.write(">lcl|%i " % sequence_id)
-                    sequence_id += 1
-                    output_stream.write(SequenceDatabase.DEFLINE_DELIMITER_CHARACTER.join(ids_to_print))
-                    output_stream.write("\n")
-                    output_stream.write(last_sequence)
-                    output_stream.write("\n")
-                ids_to_print = [d[1]]
-            last_sequence = d[0]
-        if ids_to_print != []:
-            # Print the last sequence
-            output_stream.write(">lcl|%i " % sequence_id)
-            output_stream.write(SequenceDatabase.DEFLINE_DELIMITER_CHARACTER.join(ids_to_print))
-            output_stream.write("\n")
-            output_stream.write(d[0])
-            output_stream.write("\n")
-        logging.debug("Finished writing fasta in thread")
 
     @staticmethod
     def grouper(iterable, n):
@@ -115,82 +103,51 @@ class SequenceDatabase:
                   " sequence text, num_hits int, coverage float, taxonomy text)")
         db.commit()
 
-        # 100% cluster the database.
-        sequences_fasta_file = os.path.join(db_path, "sequences.fasta")
-        with open(sequences_fasta_file, 'w') as fasta:
-            logging.info("Writing FASTA file to %s .." % sequences_fasta_file)
-            cmd = "sort -S 20%" # The default sort buffer is too low IMO, use a larger one.
-            sorter = subprocess.Popen(cmd, shell=True, stdin = subprocess.PIPE, stdout=subprocess.PIPE)
-            fasta_writing_thread = Thread(target=SequenceDatabase.write_dereplicated_fasta_file,
-                                          args=[sorter.stdout, fasta])
-            fasta_writing_thread.start()
-            chunksize = 10000 # Run in chunks for sqlite insert performance.
-            for chunk in SequenceDatabase.grouper(otu_table_collection, chunksize):
-                chunk_list = []
-                for entry in chunk:
-                    if entry is not None: # Is None when padded in last chunk.
-                        chunk_list.append((entry.marker, entry.sample_name, entry.sequence, entry.count,
-                            entry.coverage, entry.taxonomy))
-                        dbseq = DBSequence()
-                        dbseq.marker = entry.marker
-                        dbseq.sample_name = entry.sample_name
-                        dbseq.sequence = entry.sequence
-                        dbseq.count = entry.count
-                        dbseq.taxonomy = entry.taxonomy
+        gene_to_tempfile = {}
 
-                        # Replace gaps with a replacement char so we can tell the difference between - and N.
-                        if SequenceDatabase.GAP_REPLACEMENT_CHARACTER in dbseq.sequence:
-                            logging.warn("Attempting to create database with the reserved character %s, calculated divergences may be slightly incorrect" % SequenceDatabase.GAP_REPLACEMENT_CHARACTER)
-                        sorter.stdin.write(dbseq.sequence.replace('-',SequenceDatabase.GAP_REPLACEMENT_CHARACTER)+"\t")
-                        sorter.stdin.write(dbseq.fasta_defline() + "\n")
+        # logging.info("Writing FASTA file to %s .." % sequences_fasta_file)
+        # cmd = "sort -S 20%" # The default sort buffer is too low IMO, use a larger one.
+        # sorter = subprocess.Popen(cmd, shell=True, stdin = subprocess.PIPE, stdout=subprocess.PIPE)
+        # fasta_writing_thread = Thread(target=SequenceDatabase.write_dereplicated_fasta_file,
+        #                               args=[sorter.stdout, fasta])
+        # fasta_writing_thread.start()
+        chunksize = 10000 # Run in chunks for sqlite insert performance.
+        for chunk in SequenceDatabase.grouper(otu_table_collection, chunksize):
+            chunk_list = []
+            for entry in chunk:
+                if entry is not None: # Is None when padded in last chunk.
+                    chunk_list.append((entry.marker, entry.sample_name, entry.sequence, entry.count,
+                        entry.coverage, entry.taxonomy))
+                    dbseq = DBSequence()
+                    dbseq.marker = entry.marker
+                    dbseq.sample_name = entry.sample_name
+                    dbseq.sequence = entry.sequence
+                    dbseq.count = entry.count
+                    dbseq.taxonomy = entry.taxonomy
 
-                c.executemany("INSERT INTO otus(marker, sample_name, sequence, num_hits, "
-                              "coverage, taxonomy) VALUES(?,?,?,?,?,?)",
-                              chunk_list)
+                    if entry.marker not in gene_to_tempfile:
+                        gene_to_tempfile[entry.marker] = tempfile.NamedTemporaryFile(prefix='singlem-makedb')
+                    tf = gene_to_tempfile[entry.marker]
+                    tf.write("%s\n" % entry.sequence)
 
-            logging.info("Creating SQLite indices")
-            c.execute("CREATE INDEX otu_sequence on otus (sequence)")
-            c.execute("CREATE INDEX otu_sample_name on otus (sample_name)")
-            db.commit()
-            sorter.stdin.close()
-            fasta_writing_thread.join()
+            c.executemany("INSERT INTO otus(marker, sample_name, sequence, num_hits, "
+                          "coverage, taxonomy) VALUES(?,?,?,?,?,?)",
+                          chunk_list)
 
-        cmd = "makeblastdb -in '%s' -dbtype nucl -parse_seqids" % sequences_fasta_file
-        logging.info("Generating BLAST database ..")
-        extern.run(cmd)
-        logging.info("Removing intermediate file ..")
-        os.remove(sequences_fasta_file)
+        logging.info("Creating SQLite indices")
+        c.execute("CREATE INDEX otu_sequence on otus (sequence)")
+        c.execute("CREATE INDEX otu_sample_name on otus (sample_name)")
+        db.commit()
+
+        # Run smafa on each of the genomes
+        logging.info("Running smafas")
+        for marker_name, tf in gene_to_tempfile.items():
+            smafa = "%s.smafadb" % os.path.join(db_path, marker_name)
+            tf.flush()
+            cmd = "sort -S20%% '%s' |uniq |awk '{print \">1\\n\" $1}' |smafa makedb /dev/stdin '%s'" % (
+                tf.name, smafa)
+            logging.debug("Running cmd: %s", cmd)
+            logging.info("Formatting smafa database %s .." % smafa)
+            extern.run(cmd)
+            tf.close()
         logging.info("Finished")
-
-    def extract_sequences(self, sequence_id):
-        cmd = "blastdbcmd -db '%s' -entry 'lcl|%s'" %\
-            (self.sequences_fasta_file, sequence_id)
-        stdout = extern.run(cmd)
-        for s in SeqIO.parse(StringIO.StringIO(stdout), "fasta"):
-            if dbseq.sequence: raise Exception("Extracted multiple hits from sequence database, for sequence id %s" % sequence_id)
-            dbseq.parse_from_fasta_define(s.description)
-            dbseq.sequence = str(s.seq).replace(
-                SequenceDatabase.GAP_REPLACEMENT_CHARACTER,'-')
-        return dbseq
-
-    def extract_sequences_by_blast_ids(self, blast_ids):
-        num_blast_ids = 0
-        with tempfile.NamedTemporaryFile(prefix='singlem_query_for_blastdbcmd') as batchfile:
-            for bid in blast_ids:
-                batchfile.write(bid)
-                batchfile.write("\n")
-                num_blast_ids += 1
-            batchfile.flush()
-            cmd = "blastdbcmd -db '%s' -entry_batch '%s'"\
-                   % (self.sequences_fasta_file, batchfile.name)
-            stdout = extern.run(cmd)
-            dbseqs = []
-            for s in SeqIO.parse(StringIO.StringIO(stdout), "fasta"):
-                new_seqs = DBSequence.parse_from_fasta_define(s.description)
-                for single_dbseq in new_seqs:
-                    single_dbseq.sequence = str(s.seq).replace(
-                        SequenceDatabase.GAP_REPLACEMENT_CHARACTER,'-')
-                dbseqs.append(new_seqs)
-            if len(dbseqs) != num_blast_ids:
-                raise Exception("Unexpected number of returned sequences from blastdbcmd")
-        return list(itertools.chain(*dbseqs))
