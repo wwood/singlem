bcfc08de4039906c4276bb89cb9ac1535b3d0e88
Ben Woodcroft <donttrustben@gmail.com>
2018-03-25 20:33:45 +1000

    placement_parser: New class.

---
+++ singlem/placement_parser.py
@@ -0,0 +1,86 @@
+
+
+class PlacementParser:
+    def __init__(self, json, taxonomy_bihash):
+        self._json = json
+        self._taxonomy_bihash = taxonomy_bihash
+        self._sequence_to_placement = {}
+        for placement in json['placements']:
+            for nm in placement['nm']:
+                if nm[1] != 1:
+                    raise Exception("Cannot handle jplace files with nm counts != 1")
+                sequence = nm[0]
+                if sequence in self._sequence_to_placement:
+                    raise Exception(
+                        "There appears to be duplicate names amongst placed sequences")
+                self._sequence_to_placement[sequence] = placement
+
+    def otu_placement(self, sequence_names, threshold):
+        '''Return the most fully resolved taxonomy of the set of reads, pooling the
+        placement confidences of each sequence. The returned taxonomy is the
+        taxonomy whose placement probability sum is > len(sequence_names) *
+        threshold.
+
+        '''
+
+        observed_parent_to_children = {}
+        tax_probabilities = {}
+
+        j = self._json
+        classification_index = j['fields'].index('classification')
+        likelihood_index = j['fields'].index('like_weight_ratio')
+        root_tax = None
+
+        # For each placement for each sequence
+        for name in sequence_names:
+            for p in self._sequence_to_placement[name]['p']:
+                # Get list of taxonomies from placed to root of tree
+                prob = p[likelihood_index]
+                tax = p[classification_index]
+                full_tax = []
+                while tax is not None:
+                    full_tax.append(tax)
+                    tax = self._taxonomy_bihash.child_to_parent[tax]
+                # Add that probability in the total hash
+                last_parent = None
+                for i, tax in enumerate(reversed(full_tax)):
+                    if i == 0:
+                        if root_tax is None:
+                            root_tax = tax
+                        elif tax != root_tax:
+                            raise Exception(
+                                "Programming error - seem to have encountered 2 different roots")
+                    if tax in tax_probabilities:
+                        tax_probabilities[tax] += prob
+                    else:
+                        tax_probabilities[tax] = prob
+                    if i != 0:
+                        if last_parent in observed_parent_to_children:
+                            if tax not in observed_parent_to_children[last_parent]:
+                                observed_parent_to_children[last_parent].append(tax)
+                        else:
+                            observed_parent_to_children[last_parent] = [tax]
+                    last_parent = tax
+
+        # Descend down the tree, picking the highest probability amongst the
+        # childrens, or break if the probability is not greater than the
+        # threshold.
+        next_children = [root_tax]
+        final_tax = []
+        next_probability = 1.
+        while True:
+            # Find the max child and it's probability
+            def get_prob(tax): return tax_probabilities[tax]
+            max_child = max(next_children, key=get_prob)
+            # Add it to the final tax if it is above the threshold
+            if tax_probabilities[max_child] >= threshold * len(sequence_names):
+                final_tax.append(max_child)
+                if max_child in observed_parent_to_children:
+                    next_children = observed_parent_to_children[max_child]
+                else:
+                    break # If there is no children, break
+            else:
+                break # If below the threshold, break
+
+        # Return the taxonomic placement
+        return final_tax
---
+++ test/test_placement_parser.py
@@ -0,0 +1,171 @@
+
+#!/usr/bin/env python
+
+#=======================================================================
+# Authors: Ben Woodcroft, Tim Lamberton.
+#
+# Unit tests.
+#
+# Copyright
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License.
+# If not, see <http://www.gnu.org/licenses/>.
+#=======================================================================
+
+import unittest
+import subprocess
+import os.path
+import tempfile
+import tempdir
+from string import split
+from StringIO import StringIO
+import extern
+import sys
+import json
+import re
+
+path_to_script = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..','bin','singlem')
+path_to_data = os.path.join(os.path.dirname(os.path.realpath(__file__)),'data')
+
+sys.path = [os.path.join(os.path.dirname(os.path.realpath(__file__)),'..')]+sys.path
+from singlem.placement_parser import PlacementParser
+from singlem.taxonomy_bihash import TaxonomyBihash
+
+class Tests(unittest.TestCase):
+    maxDiff = None
+
+    def test_(self):
+        placement = {
+            "fields": [
+                "classification",
+                "distal_length",
+                "edge_num",
+                "like_weight_ratio",
+                "likelihood",
+                "pendant_length"
+            ],
+            "version": 3,
+            "tree": "(((((637000311:0.36651{0},(2600255113:0.30323{1},(2513237397:0.20355{2},2561511140:0.1579{3})0.364:0.0655{4})0.617:0.05106{5})1.000:0.58532{6},(2521172697:0.61844{7},(2582581027:0.32233{8},2571042868:0.25573{9})0.949:0.15258{10})0.902:0.1263{11})0.435:0.07766{12},(2585428157:0.73027{13},(2588253501:0.29907{14},(2556921088:0.11546{15},2518645585:0.12806{16})0.960:0.14579{17})0.991:0.28541{18})0.746:0.0561{19})0.105:0.19809{20},(2576861831:0.42312{21},(2528768167:0.20363{22},2524614704:0.28321{23})0.997:0.37729{24})0.755:0.07293{25})0.545:0.12653{26},(646564583:0.4755{27},(640069326:0.63132{28},650377985:0.541{29})0.722:0.26793{30})0.983:0.79001{31},(2516653045:0.31813{32},(2523533623:0.48892{33},2579778789:0.25687{34})0.782:0.15439{35})0.934:0.60024{36}){37};",
+            "placements": [
+                {
+                    "p": [
+                        [
+                            "p__Firmicutes",
+                            0.289785332337,
+                            24,
+                            0.825736915661,
+                            -1119.74125853,
+                            0.102097663521
+                        ],
+                        [
+                            "p__Firmicutes",
+                            0.389679026464,
+                            21,
+                            0.0934323316861,
+                            -1121.9202973,
+                            0.103482367825
+                        ],
+                        [
+                            "d__Bacteria",
+                            8.90258789062e-06,
+                            25,
+                            0.0808307526527,
+                            -1122.06517725,
+                            0.112723096201
+                        ]
+                    ],
+                    "nm": [
+                        [
+                            "HWI-ST1243:156:D1K83ACXX:7:1106:18671:79482_2_2_1",
+                            1
+                        ]
+                    ]
+                },
+                {
+                    "p": [
+                        [
+                            "p__Firmicutes",
+                            0.265430244293,
+                            24,
+                            0.879016471214,
+                            -1107.52673141,
+                            0.0889677089655
+                        ],
+                        [
+                            "p__Firmicutes",
+                            0.423113543701,
+                            21,
+                            0.0604937152121,
+                            -1110.20299556,
+                            0.10092473344
+                        ],
+                        [
+                            "d__Bacteria",
+                            8.90258789062e-06,
+                            25,
+                            0.0604898135741,
+                            -1110.20306006,
+                            0.100916068941
+                        ]
+                    ],
+                    "nm": [
+                        [
+                            "HWI-ST1243:156:D1K83ACXX:7:1105:19152:28331_1_4_1",
+                            1
+                        ]
+                    ]
+                }
+            ],
+            "metadata": {
+                "invocation": "pplacer -j 5 --verbosity 0 --out-dir /tmp/o -c test/data/4.11.22seqs.gpkg.spkg/4.11.22seqs/4.11.22seqs.gpkg.refpkg /tmp/o/combined_alignment.aln.fa"
+            }
+        }
+
+        taxonomy = """tax_id,parent_id,rank,tax_name,root,kingdom,phylum,class,order,family,genus,species
+Root,Root,root,Root,Root,,,,,,,
+d__Bacteria,Root,kingdom,d__Bacteria,Root,d__Bacteria,,,,,,
+p__Firmicutes,d__Bacteria,phylum,p__Firmicutes,Root,d__Bacteria,p__Firmicutes,,,,,
+c__Bacilli,p__Firmicutes,class,c__Bacilli,Root,d__Bacteria,p__Firmicutes,c__Bacilli,,,,
+c__Clostridia,p__Firmicutes,class,c__Clostridia,Root,d__Bacteria,p__Firmicutes,c__Clostridia,,,,
+o__Clostridiales,c__Clostridia,order,o__Clostridiales,Root,d__Bacteria,p__Firmicutes,c__Clostridia,o__Clostridiales,,,
+"""
+        bihash = TaxonomyBihash.parse_taxtastic_taxonomy(StringIO(taxonomy))
+        parser = PlacementParser(placement, bihash)
+        self.assertEqual(
+            ["Root",'d__Bacteria','p__Firmicutes'],
+            parser.otu_placement([
+                'HWI-ST1243:156:D1K83ACXX:7:1105:19152:28331_1_4_1',
+                'HWI-ST1243:156:D1K83ACXX:7:1106:18671:79482_2_2_1',
+                ], 0.5))
+        # Higher threshold
+        self.assertEqual(
+            ["Root",'d__Bacteria'],
+            parser.otu_placement([
+                'HWI-ST1243:156:D1K83ACXX:7:1105:19152:28331_1_4_1',
+                'HWI-ST1243:156:D1K83ACXX:7:1106:18671:79482_2_2_1',
+                ], 0.95))
+        # call Firmicutes when it isn't explicitly stated in the jplace file
+        placement['placements'][0]['p'][0][0] = 'o__Clostridiales'
+        placement['placements'][0]['p'][1][0] = 'o__Clostridiales'
+        placement['placements'][1]['p'][0][0] = 'c__Bacilli'
+        placement['placements'][1]['p'][1][0] = 'c__Bacilli'
+        parser = PlacementParser(placement, bihash)
+        self.assertEqual(
+            ["Root",'d__Bacteria','p__Firmicutes'],
+            parser.otu_placement([
+                'HWI-ST1243:156:D1K83ACXX:7:1105:19152:28331_1_4_1',
+                'HWI-ST1243:156:D1K83ACXX:7:1106:18671:79482_2_2_1',
+                ], 0.5))
+
+if __name__ == "__main__":
+    unittest.main()
