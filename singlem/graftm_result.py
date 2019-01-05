6797debc9aaebe24c46a4106519822ad45959561
Ben J Woodcroft <donttrustben@gmail.com>
2019-01-06 08:29:55 +1000

    pipe: Towards paired input.

--- singlem/graftm_result.py
+++ singlem/graftm_result.py
@@ -4,19 +4,32 @@ import os
 class GraftMResult:
     def __init__(self,
                  output_directory,
+                 analysing_pairs,
                  search_hmm_files=None):
         self.output_directory = output_directory
         self._search_hmm_files = search_hmm_files
+        self._analysing_pairs = analysing_pairs
+        if analysing_pairs != False and analysing_pairs != True:
+            raise Exception() # To help with refactoring
 
     def unaligned_sequence_paths(self, require_hits=False):
-        '''NB: Only return hits when there is at least 1 hit sequence. Return a dict of
-        sample name to hit path.'''
+        '''Return a dict of sample name to hit path, or a Dict of sample name to pair
+        of paths if analysing paired data.
+
+        NB: Only return hits when there is at least 1 hit sequence.
+
+        '''
         if not require_hits: raise NotImplementedException()
         paths = {}
         for sample in self.sample_names():
-            path = self.unaligned_sequences_path_from_sample_name(sample)
-            if os.stat(path).st_size > 0:
-                paths[sample] = path
+            if self._analysing_pairs:
+                paths2 = self.unaligned_paired_sequence_paths_from_sample_name(sample)
+                if os.stat(paths2[0]).st_size > 0 or os.stat(paths2[1]).st_size > 0:
+                    paths[sample] = paths2
+            else:
+                path = self.unaligned_sequences_path_from_sample_name(sample)
+                if os.stat(path).st_size > 0:
+                    paths[sample] = path
         return paths
 
     def unaligned_sequences_path_from_sample_name(self, sample_name):
@@ -24,6 +37,18 @@ class GraftMResult:
                             sample_name,
                             "%s_hits.fa" % sample_name)
         return path
+
+    def unaligned_paired_sequence_paths_from_sample_name(self, sample_name):
+        paths = [
+            os.path.join(self.output_directory,
+                         sample_name,
+                         'forward',
+                         "%s_forward_hits.fa" % sample_name),
+            os.path.join(self.output_directory,
+                         sample_name,
+                         'reverse',
+                         "%s_reverse_hits.fa" % sample_name)]
+        return paths
 
     def hmmout_paths_from_sample_name(self, sample_name):
         if self._search_hmm_files is None:
