c302f9f6749fc9294e1f12babc1950040c47d3e7
rzhao-2 <61304462+rzhao-2@users.noreply.github.com>
2020-09-11 15:53:33 +1000

    file extension remover in prefilter

--- singlem/pipe.py
+++ singlem/pipe.py
@@ -840,6 +840,8 @@ class SearchPipe:
         for file in read_files:
             fasta_path = os.path.join(prefilter_dir,
                                       os.path.basename(file)) #not sure how well this handles .gz
+            fasta_path = os.path.splitext(fasta_path)[0]
+            
             f = open(fasta_path, 'w+') # create tempfile in working directory
             f.close()
             
