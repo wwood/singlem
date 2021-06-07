b2c5d10021ca213dababb3a88e9f545201dc12d4
Ben Woodcroft <donttrustben@gmail.com>
2021-06-07 12:27:59 +1000

    further profiling.

--- singlem/pipe.py
+++ singlem/pipe.py
@@ -929,6 +929,7 @@ class SearchPipe:
             search_result.samples_with_hits(),
             analysing_pairs)
 
+    @profile
     def _assign_taxonomy(self, extracted_reads, assignment_method, assignment_threads,
         diamond_taxonomy_assignment_performance_parameters):
         
@@ -1403,9 +1404,10 @@ if __name__ == '__main__':
         'sequences': None,
         'reverse_read_files': None,
         'genomes': None,
-        'input_sra_files': ['/home/woodcrob/s/59_large_run_fix3/ERR2092774.sra'],
+        'input_sra_files': ['/home/woodcrob/s/59_large_run_fix3/ERR1877729.sra'], #small
+        # 'input_sra_files': ['/home/woodcrob/s/59_large_run_fix3/ERR2092774.sra'], # 58GB big
         'otu_table': None,
-        'archive_otu_table': '/home/woodcrob/s/59_large_run_fix3/ERR2092774.sra-mprof.json',
+        'archive_otu_table': '/home/woodcrob/s/59_large_run_fix3/blah.sra-mprof.json',
         'threads': 1,
         'known_otu_tables': None,
         'assignment_method': 'diamond',
