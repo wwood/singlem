3332c8cac695e75b920d18ada4afe9fa7dbc0d6d
Ben Woodcroft <benjwoodcroft@gmail.com>
2021-08-04 10:07:34 +1000

    StreamingOtus: Fix each_sample_otus bug, condense: abstract.

--- test/test_condense.py
+++ test/test_condense.py
@@ -26,21 +26,25 @@ import os.path
 import tempdir
 import sys
 import io
-
-from singlem.condense import Condenser
-
-path_to_data = os.path.join(os.path.dirname(os.path.realpath(__file__)),'data','condense')
+import extern
 
 sys.path = [os.path.join(os.path.dirname(os.path.realpath(__file__)),'..')]+sys.path
+from singlem.condense import Condenser
+from singlem.otu_table_collection import StreamingOtuTableCollection
+
+path_to_script = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..','bin','singlem')
+path_to_data = os.path.join(os.path.dirname(os.path.realpath(__file__)),'data','condense')
 
 class Tests(unittest.TestCase):
     maxDiff = None
     
     def test_condense_small(self): #TODO add singlem packages, example input and output comparators
         with tempdir.in_tempdir():
+            stream = StreamingOtuTableCollection()
+            stream.add_otu_table_file(os.path.join(
+                path_to_data, 'small_condense_input.csv'))
             Condenser().condense(
-                input_otu_table = os.path.join(
-                    path_to_data, 'small_condense_input.csv'),
+                input_streaming_otu_table = stream,
                 singlem_packages = [
                     os.path.join(path_to_data, 'S2.1.ribosomal_protein_L2_rplB.gpkg.spkg'),
                     os.path.join(path_to_data, 'S2.10.ribosomal_protein_S7.gpkg.spkg'),
@@ -50,6 +54,17 @@ class Tests(unittest.TestCase):
                 krona = '/dev/null')
             with open('small_condense_output.csv') as observed:
                 with open(os.path.join(path_to_data, 'small_condense_output.csv')) as expected:
+                    self.assertListEqual(list(observed), list(expected))
+    
+    def test_condense_two_tables_cmdline(self): #TODO add singlem packages, example input and output comparators
+        with tempdir.in_tempdir():
+            cmd = "{} condense --input-otu-tables {}/small_condense_input_another.csv --output-otu-table out --krona a.html --singlem-packages {}/*spkg".format(
+                path_to_script, path_to_data, path_to_data, path_to_data
+            )
+            observed = extern.run(cmd)
+            
+            with open('out') as observed:
+                with open(os.path.join(path_to_data, 'small_condense_output_2_samples.csv')) as expected:
                     self.assertListEqual(list(observed), list(expected))
 
 if __name__ == "__main__":
