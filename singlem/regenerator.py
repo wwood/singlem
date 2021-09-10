85692a7b271f2ea19922d5b5e2b8d5a8232d17fd
AroneyS <samuel.aroney@qut.edu.au>
2021-09-10 11:20:34 +1000

    add initial regenerator improvement
    takes input_singlem_package and updates with:
    input_sequences
    input_taxonomy
    euk_sequences
    euk_taxonomy
    
    output: output_singlem_package

--- singlem/regenerator.py
+++ singlem/regenerator.py
@@ -1,6 +1,6 @@
 import logging
 import os
-
+import tempfile
 import extern
 import dendropy
 from graftm.graftm_package import GraftMPackage
@@ -16,12 +16,10 @@ class Regenerator:
     def regenerate(self, **kwargs):
         input_singlem_package = kwargs.pop('input_singlem_package')
         output_singlem_package = kwargs.pop('output_singlem_package')
-        working_directory = kwargs.pop('working_directory')
+        input_sequences = kwargs.pop('input_sequences')
+        input_taxonomy = kwargs.pop('input_taxonomy')
         euk_sequences = kwargs.pop('euk_sequences')
         euk_taxonomy = kwargs.pop('euk_taxonomy')
-        intermediate_archaea_graftm_package = kwargs.pop('intermediate_archaea_graftm_package')
-        intermediate_bacteria_graftm_package = kwargs.pop('intermediate_bacteria_graftm_package')
-        input_taxonomy = kwargs.pop('input_taxonomy')
         min_aligned_percent = kwargs.pop('min_aligned_percent')
 
         if len(kwargs) > 0:
@@ -29,7 +27,13 @@ class Regenerator:
 
         original_pkg = SingleMPackage.acquire(input_singlem_package)
         original_hmm_path = original_pkg.hmm_path()
+        original_hmm_search_paths = original_pkg.graftm_package().search_hmm_paths()
         basename = original_pkg.graftm_package_basename()
+
+        tmp = tempfile.TemporaryDirectory()
+        working_directory = tmp.name
+        logging.debug("Using working directory %s" % working_directory)
+
 
         # Run GraftM on the euk sequences with the bacterial set
         euk_graftm_output = os.path.join(working_directory,
@@ -47,11 +51,7 @@ class Regenerator:
                 "Unexpected number of hits against euk in graftm")
         euk_hits_path = next(iter(hit_paths.values())) #i.e. first
 
-        # Concatenate euk, archaea and bacterial sequences
-        archaeal_intermediate_pkg = GraftMPackage.acquire(
-            intermediate_archaea_graftm_package)
-        bacterial_intermediate_pkg = GraftMPackage.acquire(
-            intermediate_bacteria_graftm_package)
+        # Concatenate input (bacteria and archaea) and euk sequences
         num_euk_hits = 0
         final_sequences_path = os.path.join(working_directory,
                                             "%s_final_sequences.faa" % basename)
@@ -64,19 +64,8 @@ class Regenerator:
                         final_seqs_fp.write(">%s\n%s\n" % (name, seq))
             logging.info("Found %i eukaryotic sequences to include in the package" % \
                          num_euk_hits)
-
-            for gpkg in [archaeal_intermediate_pkg, bacterial_intermediate_pkg]:
-                num_total = 0
-                num_written = 0
-                with open(gpkg.unaligned_sequence_database_path()) as seqs:
-                    for name, seq, _ in SeqReader().readfq(seqs):
-                        num_total += 1
-                        # if name in species_dereplicated_ids:
-                        final_seqs_fp.write(">%s\n%s\n" % (name, seq))
-                        num_written += 1
-                logging.info(
-                    "Of %i sequences in gpkg %s, %i species-dereplicated were included in the final package." %(
-                        num_total, gpkg, num_written))
+        
+        extern.run("cat %s >> %s" % (input_sequences, final_sequences_path))
 
         # Concatenate euk and input taxonomy
         final_taxonomy_file = os.path.join(working_directory,
@@ -87,59 +76,15 @@ class Regenerator:
         # Run graftm create to get the final package
         final_gpkg = os.path.join(working_directory,
                                   "%s_final.gpkg" % basename)
-        cmd = "graftM create --force --min_aligned_percent %s --sequences %s --taxonomy %s --search_hmm_files %s %s --hmm %s --output %s" % (
+        cmd = "graftM create --no-tree --force --min_aligned_percent %s --sequences %s --taxonomy %s --search_hmm_files %s --hmm %s --output %s" % (
             min_aligned_percent,
             final_sequences_path,
             final_taxonomy_file,
-            ' '.join(archaeal_intermediate_pkg.search_hmm_paths()),
-            ' '.join(bacterial_intermediate_pkg.search_hmm_paths()),
+            ' '.join(original_hmm_search_paths),
             original_hmm_path,
             final_gpkg)
-        try:
-            extern.run(cmd)
-        except Exception:
-            logging.info("Automatically retrying graftM after taxit create failure")
-            
-            rerooted_tree = "graftm_create_tree." + basename.split(".")[0] + ".tree"
-            
-            cmd = "graftM create --force --min_aligned_percent %s --sequences %s --taxonomy %s --search_hmm_files %s %s --hmm %s --output %s --rerooted_tree %s" % (
-            min_aligned_percent,
-            final_sequences_path,
-            final_taxonomy_file,
-            ' '.join(archaeal_intermediate_pkg.search_hmm_paths()),
-            ' '.join(bacterial_intermediate_pkg.search_hmm_paths()),
-            original_hmm_path,
-            final_gpkg,
-            rerooted_tree)
-            extern.run(cmd)
 
-        ##############################################################################
-        # Remove sequences from the diamond DB that are not in the tree i.e.
-        # those that are exact duplicates, so that the diamond_example hits are
-        # always in the tree.
-        # Read the list of IDs in the tree with dendropy
-        final_gpkg_object = GraftMPackage.acquire(final_gpkg)
-        unaligned_seqs = final_gpkg_object.unaligned_sequence_database_path()
-        tree = dendropy.Tree.get(path=final_gpkg_object.reference_package_tree_path(),
-                                 schema='newick')
-        leaf_names = [l.taxon.label.replace(' ','_') for l in tree.leaf_node_iter()]
-        logging.debug("Read in final tree with %i leaves" % len(leaf_names))
-
-        # Extract out of the sequences file in the graftm package
-        final_seqs = SequenceExtractor().extract_and_read(
-            leaf_names, unaligned_seqs)
-        if len(final_seqs) != len(leaf_names):
-            raise Exception("Do not appear to have extracted the expected number of sequences from the unaligned fasta file")
-
-        # Write the reads into sequences file in place
-        with open(unaligned_seqs, 'w') as f:
-            for s in final_seqs:
-                f.write(">%s\n" % s.name)
-                f.write(s.seq)
-                f.write("\n")
-
-        # Regenerate the diamond DB
-        final_gpkg_object.create_diamond_db()
+        extern.run(cmd)
 
         ##############################################################################
         # Run singlem create to put the final package together
---
+++ test/test_regenerator.py
@@ -0,0 +1,99 @@
+
+#!/usr/bin/env python3
+
+#=======================================================================
+# Authors: Ben Woodcroft, Samuel Aroney.
+#
+# Unit tests.
+#
+# Copyright
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License.
+# If not, see <http://www.gnu.org/licenses/>.
+#=======================================================================
+
+from singlem.singlem_package import SingleMPackage
+import unittest
+from unittest.mock import Mock,patch
+import os
+import sys
+from bird_tool_utils import in_tempdir
+import extern
+import io
+
+path_to_script = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..','bin','singlem')
+path_to_data = os.path.join(os.path.dirname(os.path.realpath(__file__)),'data')
+
+sys.path = [os.path.join(os.path.dirname(os.path.realpath(__file__)),'..')]+sys.path
+from singlem.regenerator import Regenerator
+
+class Tests(unittest.TestCase):
+    @unittest.skip("mocks don't work on extern run commands")
+    def test_hello_word_cmdline(self):
+        with patch('singlem.regenerator') as mock:
+            mock.regenerate.return_value = 'result'
+
+            cmd = "{} regenerate ".format(path_to_script)
+            cmd += "--input_singlem_package {} ".format(path_to_data)
+            cmd += "--output_singlem_package {} ".format(path_to_data)
+            cmd += "--working_directory {} ".format(path_to_data)
+            cmd += "--euk_sequences {} ".format(path_to_data)
+            cmd += "--euk_taxonomy {} ".format(path_to_data)
+            cmd += "--intermediate_archaea_graftm_package {} ".format(path_to_data)
+            cmd += "--intermediate_bacteria_graftm_package {} ".format(path_to_data)
+            cmd += "--input_taxonomy {} ".format(path_to_data)
+            cmd += "--min_aligned_percent 10 "
+            output = extern.run(cmd)
+
+            mock.regenerate.assert_called_once()
+    
+    def test_regenerate_package(self):
+        with in_tempdir():
+            # small package with few sequences
+            input_package = os.path.join(path_to_data, "4.11.22seqs.v3.gpkg.spkg")
+            # same sequences with altered taxonomy (rotated by 1)
+            input_sequences = os.path.join(path_to_data, "regenerate", "input_sequences.fasta")
+            input_taxonomy = os.path.join(path_to_data, "regenerate", "input_taxonomy.tsv")
+            # few sequences from uniprot that match input package
+            euk_sequences = os.path.join(path_to_data, "regenerate", "uniprot_sprot_hits.fa")
+            euk_taxonomy = os.path.join(path_to_data, "regenerate", "uniprot_sprot_taxonomy.tsv")
+
+            output_package = "regenerated.spkg"
+            window_position = 20
+            min_aligned_percent = 1
+
+            
+            Regenerator().regenerate(
+                    input_singlem_package = input_package,
+                    input_sequences = input_sequences,
+                    input_taxonomy = input_taxonomy,
+                    euk_sequences = euk_sequences,
+                    euk_taxonomy = euk_taxonomy,
+                    output_singlem_package = output_package,
+                    min_aligned_percent = min_aligned_percent)
+
+            pkg = SingleMPackage.acquire(output_package)
+            #self.assertEqual(window_position, pkg.singlem_position())
+
+            # assert sequences and taxonomy have been supplemented with euk sequences and updated
+            observed_output_fasta = list(io.open(os.path.join(output_package, "regenerated", "4.11.22seqs_final_sequences.faa")))
+            expected_output_fasta = list(io.open(os.path.join(path_to_data, "regenerate", "output_full.fasta")))
+            self.assertListEqual(observed_output_fasta, expected_output_fasta)
+
+            observed_output_seqinfo = list(io.open(os.path.join(output_package, "regenerated", "4.11.22seqs_final.gpkg.refpkg", "4_seqinfo.csv")))
+            expected_output_seqinfo = list(io.open(os.path.join(path_to_data, "regenerate", "output_seqinfo.csv")))
+            self.assertListEqual(observed_output_seqinfo, expected_output_seqinfo)
+
+
+if __name__ == "__main__":
+    unittest.main()
