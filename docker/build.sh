e543314ba5a372d7bab08e572d59bb6bdede3acc
Ben Woodcroft <donttrustben@gmail.com>
2022-10-15 07:02:40 +1000

    docker: wwood/singlem:1.0.0beta1

--- docker/build.sh
+++ docker/build.sh
@@ -3,11 +3,7 @@
 
 export SINGLEM_VERSION=wwood/singlem:1.0.0beta1
 
-# Copy env
-cp ../singlem.yml .
-
-DOCKER_BUILDKIT=1 docker build -t $SINGLEM_VERSION .
-
-docker run -v `pwd`:`pwd` $SINGLEM_VERSION pipe --sequences `pwd`/test.fna --otu-table /dev/stdout
-
+cp ../singlem.yml . && \
+DOCKER_BUILDKIT=1 docker build -t $SINGLEM_VERSION . && \
+docker run -v `pwd`:`pwd` $SINGLEM_VERSION pipe --sequences `pwd`/test.fna --otu-table /dev/stdout && \
 echo "Remember to change version numbers and metapackage path in Dockerfile .. then 'docker push $SINGLEM_VERSION'"
