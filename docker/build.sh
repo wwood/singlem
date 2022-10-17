b8a92ed15bbdad8f467ba66f8967eb6552077cdd
Ben Woodcroft <donttrustben@gmail.com>
2022-10-17 10:36:12 +1000

    docker: Improve user build instructions.

--- docker/build.sh
+++ docker/build.sh
@@ -8,4 +8,4 @@ cp ../singlem.yml . && \
 sed 's/SINGLEM_VERSION/'$SINGLEM_VERSION'/g' Dockerfile.in > Dockerfile && \
 DOCKER_BUILDKIT=1 docker build -t $SINGLEM_DOCKER_VERSION . && \
 docker run -v `pwd`:`pwd` $SINGLEM_DOCKER_VERSION pipe --sequences `pwd`/test.fna --otu-table /dev/stdout && \
-echo "Remember to change version numbers and metapackage path in Dockerfile .. then 'docker push $SINGLEM_DOCKER_VERSION'"
+echo "Seems good - now you just need to 'docker push $SINGLEM_DOCKER_VERSION'"
