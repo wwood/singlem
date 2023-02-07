38a235e36ccd7abd7d81b69122b2f9c8a1a0f007
Ben Woodcroft <donttrustben@gmail.com>
2023-02-07 21:55:57 +1000

    docker: Auto-generate DB path in Dockerfile.

--- docker/build.sh
+++ docker/build.sh
@@ -3,9 +3,10 @@
 
 export SINGLEM_VERSION=`../bin/singlem --version`
 export SINGLEM_DOCKER_VERSION=wwood/singlem:$SINGLEM_VERSION
+export SINGLEM_DB_BASENAME=`basename $SINGLEM_METAPACKAGE_PATH`
 
 cp ../singlem.yml . && \
-sed 's/SINGLEM_VERSION/'$SINGLEM_VERSION'/g' Dockerfile.in > Dockerfile && \
+sed 's/SINGLEM_VERSION/'$SINGLEM_VERSION'/g; s/SINGLEM_DB_BASENAME/'$SINGLEM_DB_BASENAME'/g' Dockerfile.in > Dockerfile && \
 DOCKER_BUILDKIT=1 docker build -t $SINGLEM_DOCKER_VERSION . && \
 docker run -v `pwd`:`pwd` $SINGLEM_DOCKER_VERSION pipe --sequences `pwd`/test.fna --otu-table /dev/stdout && \
 echo "Seems good - now you just need to 'docker push $SINGLEM_DOCKER_VERSION'"
