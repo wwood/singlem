d0ca5f968b27e15c6b27652c8033d931c8b69c8c
Ben Woodcroft <benjwoodcroft@gmail.com>
2023-04-19 12:17:04 +1000

    supplement: Fix bug, add GTDBtk functionality.

--- extras/supplement_metapackage/test/test_supplement_metapackage.py
+++ extras/supplement_metapackage/test/test_supplement_metapackage.py
@@ -35,10 +35,52 @@ path_to_data = os.path.join(os.path.dirname(__file__), 'data')
 singlem_base_directory = os.path.join(os.path.dirname(__file__), '..', '..', '..')
 singlem_bin_directory = os.path.join(singlem_base_directory, 'bin')
 
+# TODO: Once GTDBtk can be included in conda env (as of diamond 2.1.7 likely), remove the added PATH entry
+run = f"PATH=$PATH:{singlem_bin_directory}:~/e/gtdbtk-v2.2.6/bin PYTHONPATH=$PYTHONPATH:{singlem_base_directory} {path_to_script} metapackage"
+singlem = f"{singlem_bin_directory}/singlem"
+
 class Tests(unittest.TestCase):
+    headers = str.split('gene sample sequence num_hits coverage taxonomy')
+    headers_with_extras = headers + str.split('read_names nucleotides_aligned taxonomy_by_known? read_unaligned_sequences equal_best_hit_taxonomies taxonomy_assignment_method')
+    maxDiff = None
+    two_packages = '%s %s' % (
+        os.path.join(path_to_data, '4.11.22seqs.gpkg.spkg'),
+        os.path.join(path_to_data, '4.12.22seqs.spkg'))
+
+    def assertEqualOtuTable(self, expected_array, observed_string):
+        observed_array = list([line.split("\t") for line in observed_string.split("\n")])
+        if expected_array[-1] != ['']:
+            expected_array.append([''])
+
+        # make sure headers are OK
+        self.assertEqual(expected_array[0], observed_array[0])
+
+        # sort the rest of the table and compare that
+        self.assertEqual(sorted(expected_array[1:]), sorted(observed_array[1:]))
+
     def test_defined_taxonomy(self):
         with in_tempdir():
-            extern.run(f"PATH=$PATH:{singlem_bin_directory} PYTHONPATH=$PYTHONPATH:{singlem_base_directory} {path_to_script} metapackage --new-genome-fasta-files {path_to_data}/GCA_011373445.1_genomic.fna --input-metapackage {path_to_data}/4.11.22seqs.gpkg.spkg.smpkg/ --output-metapackage out.smpkg --new-taxonomies {path_to_data}/GCA_011373445.1_genomic.fna.taxonomy")
+            cmd = f"{run} --new-genome-fasta-files {path_to_data}/GCA_011373445.1_genomic.mutated93_ms.fna --input-metapackage {path_to_data}/4.11.22seqs.gpkg.spkg.smpkg/ --output-metapackage out.smpkg --new-taxonomies {path_to_data}/GCA_011373445.1_genomic.mutated93_ms.fna.taxonomy"
+            extern.run(cmd)
+
+            cmd2 = f'{singlem} pipe --genome-fasta-files {path_to_data}/GCA_011373445.1_genomic.mutated93_ms.fna --metapackage out.smpkg/ --otu-table /dev/stdout'
+            output = extern.run(cmd2)
+            expected = [
+                "\t".join(self.headers),
+                '4.11.22seqs	GCA_011373445.1_genomic.mutated93_ms	CTTAAAAAGAAACTAAAAGGT------GCTCACATGAGGGTTCTAAAAAACACTCTAATT	1	1.16	Root; d__Archaea; p__Thermoproteota; c__Bathyarchaeia; o__B26-1; f__UBA233; g__DRVV01; s__NEW_SPECIES']
+            self.assertEqualOtuTable(list([line.split("\t") for line in expected]), output)
+
+    def test_auto_taxonomy(self):
+        with in_tempdir():
+            cmd = f"{run} --new-genome-fasta-files {path_to_data}/GCA_011373445.1_genomic.mutated93_ms.fna --input-metapackage {path_to_data}/4.11.22seqs.gpkg.spkg.smpkg/ --output-metapackage out.smpkg"
+            extern.run(cmd)
+
+            cmd2 = f'{singlem} pipe --genome-fasta-files {path_to_data}/GCA_011373445.1_genomic.mutated93_ms.fna --metapackage out.smpkg/ --otu-table /dev/stdout'
+            output = extern.run(cmd2)
+            expected = [
+                "\t".join(self.headers),
+                '4.11.22seqs	GCA_011373445.1_genomic.mutated93_ms	CTTAAAAAGAAACTAAAAGGT------GCTCACATGAGGGTTCTAAAAAACACTCTAATT	1	1.16	Root; d__Archaea; p__Thermoproteota; c__Bathyarchaeia; o__B26-1; f__UBA233; g__DRVV01; s__GCA_011373445.1_genomic.mutated93_ms']
+            self.assertEqualOtuTable(list([line.split("\t") for line in expected]), output)
 
 
 if __name__ == "__main__":
