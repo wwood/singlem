50656e9a79da4458881233a47110308c178f1f5c
Ben Woodcroft <donttrustben@gmail.com>
2023-04-24 14:34:27 +1000

    pipe: genome input derep: Towards it.

--- test/test_pipe.py
+++ test/test_pipe.py
@@ -1151,8 +1151,18 @@ CGGGATGTAGGCAGTGACCTCCACGCCTGAGGAGAGCCGGACGCGTGCGACCTTGCGCAACGCCGAGTTCGGCTTCTTCG
         cmd = f"{path_to_script} pipe --forward {path_to_data}/4.11.22seqs.gpkg.spkg_inseqs.fna --assignment-method diamond --singlem-package {path_to_data}/4.11.22seqs.v3_archaea_targetted.gpkg.spkg --otu-table /dev/stdout --exclude-off-target-hits"
         self.assertEqualOtuTable(with_exclude, extern.run(cmd))
 
-
-
+    def test_genome_input_dereplication(self):
+        expected = ['gene    sample  sequence        num_hits        coverage        taxonomy',
+            '4.12.22seqs     GCA_000309865.1_genomic GATGGCGGTAAAGCCACTCCCGGCCCACCATTAGGTCCAGCAATCGGACCCCTAGGTATC    2       2.39']
+        # ~/git/singlem/bin/singlem pipe --genome-fasta-files genomes/GCA_000309865.1_genomic.fna --singlem-package ../4.12.22seqs.spkg/ --otu-table /dev/stdout --no-assign-taxonomy --min-orf-length 96
+        cmd = '{} pipe --genome-fasta-files {} --singlem-package {} --otu-table /dev/stdout --no-assign-taxonomy --min-orf-length 96'.format(
+            path_to_script,
+            os.path.join(path_to_data, 'methanobacteria/genomes/GCA_000309865.1_genomic.fna'),
+            os.path.join(path_to_data, '4.12.22seqs.spkg'),
+        )
+        self.assertEqualOtuTable(
+            list([line.split("\t") for line in expected]),
+            extern.run(cmd))
 
 
 
