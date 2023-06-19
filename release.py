97843aa7a0b322f923d72c9d19410db3b834f410
Ben Woodcroft <benjwoodcroft@gmail.com>
2023-06-19 11:48:55 +1000

    test_outside_ci.py: New file.

--- release.py
+++ release.py
@@ -4,26 +4,35 @@ import io
 from os.path import dirname, join
 import extern
 
+
 def get_version(relpath):
-  """Read version info from a file without importing it"""
-  for line in io.open(join(dirname(__file__), relpath), encoding="cp437"):
-    if "__version__" in line:
-      if '"' in line:
-        return line.split('"')[1]
-      elif "'" in line:
-        return line.split("'")[1]
+    """Read version info from a file without importing it"""
+    for line in io.open(join(dirname(__file__), relpath), encoding="cp437"):
+        if "__version__" in line:
+            if '"' in line:
+                return line.split('"')[1]
+            elif "'" in line:
+                return line.split("'")[1]
+
 
 if __name__ == "__main__":
     version = get_version('singlem/version.py')
     print("version is {}".format(version))
 
+    yes_no = input(
+        "Did you run the non-CI tests first, to make sure everything is OK (y/n)? \n\npytest test/test_outside_ci.py\n\n"
+    )
+    if yes_no != "y":
+        raise Exception("Please run the non-CI tests first")
+
     print("building docs")
     extern.run("python3 build_docs.py")
 
-    print("Checking if repo is clean. If this fails it might be because the docs have changed from the previous command here?")
+    print(
+        "Checking if repo is clean. If this fails it might be because the docs have changed from the previous command here?"
+    )
     extern.run('if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then exit 1; fi')
 
     extern.run('git tag v{}'.format(version))
     print("Now run 'git push && git push --tags' and GitHub actions will build and upload to PyPI".format(version))
     print('You have to run ./build.sh from the docker directory to build the docker image, once the tag is on GitHub')
-
