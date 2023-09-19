dd9b4d00dda74b53234268ed8f6d8b62e749f6ac
Ben Woodcroft <benjwoodcroft@gmail.com>
2023-09-20 08:19:04 +1000

    supplement: Fix genome_size part, tests.

--- test/test_outside_ci.py
+++ test/test_outside_ci.py
@@ -26,12 +26,15 @@ import os.path
 import sys
 import extern
 import re
+import polars as pl
 
 import pytest
 
 sys.path = [os.path.join(os.path.dirname(os.path.realpath(__file__)),'..')]+sys.path
 
 from bird_tool_utils import in_tempdir
+
+from singlem.metapackage import Metapackage
 
 path_to_script = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..','bin','singlem')
 path_to_data = os.path.join(os.path.dirname(os.path.realpath(__file__)),'data')
@@ -45,6 +48,7 @@ singlem = f"{singlem_bin_directory}/singlem"
 class Tests(unittest.TestCase):
     '''Tests which require a full metapackage installation to run, so can't be run through GitHub actions.'''
     maxDiff = None
+    otu_table_headers = str.split('gene sample sequence num_hits coverage taxonomy')
 
     # Used in test_outside_ci.py too
     def assert_equal_taxonomic_profile(self, observed_string, expected_array_or_string):
@@ -66,6 +70,17 @@ class Tests(unittest.TestCase):
 
         # sort the rest of the table and compare that
         self.assertEqual(sorted(expected_array[1:]), sorted(observed_array[1:]))
+
+    def assertEqualOtuTable(self, expected_array, observed_string):
+        observed_array = list([line.split("\t") for line in observed_string.split("\n")])
+        if expected_array[-1] != ['']:
+            expected_array.append([''])
+
+        # make sure headers are OK
+        self.assertEqual(expected_array[0], observed_array[0])
+
+        # sort the rest of the table and compare that
+        self.assertEqual(sorted(expected_array[1:]), sorted(observed_array[1:]))        
 
     @pytest.mark.skipif(os.environ.get("SINGLEM_METAPACKAGE_PATH") is None, reason="Appear to be running in CI")
     def test_condense_cli(self):
@@ -89,15 +104,32 @@ class Tests(unittest.TestCase):
             cmd = f"{run_supplement} --no-dereplication --skip-taxonomy-check --hmmsearch-evalue 1e-5 --no-quality-filter --new-genome-fasta-files {path_to_data}/supplement/GCA_011373445.1_genomic.mutated93_ms.manually_added_nongaps.fna --output-metapackage out.smpkg --new-taxonomies {path_to_data}/supplement/GCA_011373445.1_genomic.mutated93_ms.manually_added_nongaps.fna.taxonomy --checkm2-quality-file ~/git/singlem/test/data/supplement/checkm2.output/quality_report.tsv"
             extern.run(cmd)
 
-            cmd2 = f'{singlem} pipe --genome-fasta-files {path_to_data}/GCA_011373445.1_genomic.mutated93_ms.manually_added_nongaps.fna --metapackage out.smpkg/ --otu-table /dev/stdout'
+            cmd2 = f'{path_to_script} pipe --genome-fasta-files {path_to_data}/supplement/GCA_011373445.1_genomic.mutated93_ms.manually_added_nongaps.fna --metapackage out.smpkg/ --otu-table /dev/stdout'
             output = extern.run(cmd2)
             expected = [
-                "\t".join(self.headers),
+                "\t".join(self.otu_table_headers),
                 '4.11.22seqs	GCA_011373445.1_genomic.mutated93_ms.manually_added_nongaps	CTTAAAAAGAAACTAAAAGGTGCCGGCGCTCACATGAGGGTTCTAAAAAACACTCTAATT	1	1.18	Root; d__Archaea; p__Thermoproteota; c__Bathyarchaeia; o__B26-1; f__UBA233; g__DRVV01; s__NEW_SPECIES'
             ]
-            self.assertEqualOtuTable(list([line.split("\t") for line in expected]), output)
 
-            import IPython; IPython.embed()
+            new_mpkg = Metapackage.acquire('out.smpkg')
+            new_lengths = pl.DataFrame(new_mpkg.taxon_genome_lengths())
+            new_species_entry = new_lengths.filter(pl.col('rank')=='s__NEW_SPECIES')
+            self.assertEqual(new_species_entry.shape[0], 1)
+            self.assertEqual(new_species_entry['genome_size'][0], 3937114.9507050873)
+
+            old_mpkg = Metapackage.acquire(os.environ['SINGLEM_METAPACKAGE_PATH'])
+            old_taxon_lengths = pl.DataFrame(old_mpkg.taxon_genome_lengths())
+            self.assertEqual(len(old_taxon_lengths.filter(pl.col('rank')=='s__NEW_SPECIES')), 0)
+            self.assertEqual(len(old_taxon_lengths)+1, len(new_lengths))
+
+            self.assertEqual(
+                old_taxon_lengths.filter(pl.col('rank')=='d__Bacteria')['genome_size'][0],
+                new_lengths.filter(pl.col('rank')=='d__Bacteria')['genome_size'][0]) 
+
+            self.assertTrue(
+                old_taxon_lengths.filter(pl.col('rank')=='d__Archaea')['genome_size'][0] < new_lengths.filter(pl.col('rank')=='d__Archaea')['genome_size'][0]) 
+            self.assertTrue(
+                new_lengths.filter(pl.col('rank')=='d__Archaea')['genome_size'][0] < 1.6e6)
 
 
 if __name__ == "__main__":
