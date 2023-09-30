9c89b2ff95b4f6c75857931b50038aa7bb74b382
Ben Woodcroft <benjwoodcroft@gmail.com>
2023-10-01 07:45:30 +1000

    pipe: Further adjustments for non-standard translation tables.

--- test/test_pipe.py
+++ test/test_pipe.py
@@ -1169,6 +1169,13 @@ CGGGATGTAGGCAGTGACCTCCACGCCTGAGGAGAGCCGGACGCGTGCGACCTTGCGCAACGCCGAGTTCGGCTTCTTCG
             extern.run(cmd),
             no_assign_taxonomy=True)
 
+    def test_translation_table4(self):
+        expected = 'gene    sample  sequence        num_hits        coverage        taxonomy\n' \
+            'S1.2.ribosomal_protein_L3_rplC  tt4_s1.2        ATAAACTTAATAGGTACATCAAAAGGTAAAGGTTTTCAATGAGTTATGAAAAGATTTCAT    1       1.11    Root'
+        cmd = f'{path_to_script} pipe --forward {path_to_data}/tt4_s1.2.fna --otu-table /dev/stdout --no-diamond-prefilter --translation-table 4 --threads 32 --singlem-package {path_to_data}/S1.2.ribosomal_protein_L3_rplC.gpkg.spkg/ --assignment-method diamond'
+        self.assertEqualOtuTable(
+            expected,
+            extern.run(cmd))
 
 
 
