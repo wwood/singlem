1272ab38a9928f4e97ed3df9c5ea32403c1582d1
Ben Woodcroft <benjwoodcroft@gmail.com>
2023-10-09 10:33:16 +1000

    Prepare for v0.15.0.

--- extras/update_metapackage/scripts/genome_size_from_gtdb.py
+++ extras/update_metapackage/scripts/genome_size_from_gtdb.py
@@ -33,7 +33,9 @@ import sys
 import os
 import polars as pl
 
-sys.path = [os.path.join(os.path.dirname(os.path.realpath(__file__)),'..')] + sys.path
+sys.path = [os.path.join(os.path.dirname(os.path.realpath(__file__)),'..','..','..')] + sys.path
+
+from singlem.genome_size import GenomeSizes
 
 if __name__ == '__main__':
     parent_parser = argparse.ArgumentParser(add_help=False)
@@ -70,69 +72,33 @@ if __name__ == '__main__':
 
     # Read GTDB metadata
     columns_of_interest = [
-        "accession", "gtdb_taxonomy", "gtdb_representative", 'genome_size'
+        "accession", 'gtdb_representative', "gtdb_taxonomy", 'genome_size'
     ]
     gtdb = pl.concat([
         pl.read_csv(args.gtdb_bac_metadata, separator="\t", infer_schema_length=10000000).select(columns_of_interest),
         pl.read_csv(args.gtdb_ar_metadata, separator="\t", infer_schema_length=10000000).select(columns_of_interest)
     ])
-    logging.info("Read in {} GTDB reps".format(len(gtdb)))
+    logging.info("Read in {} GTDB entries".format(len(gtdb)))
 
     # We do not calculate the checkm stats for all genomes in GTDB, just the reps, to save effort.
     gtdb_reps = gtdb.filter(pl.col("gtdb_representative") == "t")
-    gtdb_non_reps = gtdb.filter(pl.col("gtdb_representative") == "f")
-    if len(gtdb_reps) + len(gtdb_non_reps) != len(gtdb):
-        raise Exception("Unexpected GTDB metadata")
 
     # Calculate the mean completeness and contamination for each GTDB rep
     gc_reps = gtdb_reps.join(checkm, on="accession", how="inner")
     if len(gc_reps) != len(checkm):
-        raise Exception()
+        raise Exception("Different number of checkm2 stats and GTDB reps detected, something is amiss.")
+    logging.info("Joined {} GTDB reps with checkm2 stats".format(len(gc_reps)))
+
+    def correct_size(struct: dict):
+        return GenomeSizes.corrected_genome_size(struct['genome_size'], struct['Completeness']/100., struct['Contamination']/100.)
+
     gc_reps = gc_reps.with_columns(
-        (pl.col('genome_size')/(1+pl.col('Contamination')/100.)/(pl.col('Completeness')/100.)).alias('checkm2_adjusted_genome_size')
+        pl.struct(['genome_size', 'Completeness', 'Contamination']).map_elements(correct_size).alias('checkm2_adjusted_genome_size')
     )
 
-    # Trust the genome sizes for the rest as-is
-    gc_non_reps = gtdb_non_reps.with_columns(
-        pl.col('genome_size').alias('checkm2_adjusted_genome_size').cast(pl.Float64)
+    final = GenomeSizes.calculate_rank_genome_sizes(
+        list(gc_reps['gtdb_taxonomy']),
+        list(gc_reps['checkm2_adjusted_genome_size'])
     )
-
-    gc = pl.concat([
-        gc_reps.select(['accession', 'checkm2_adjusted_genome_size', 'gtdb_taxonomy']),
-        gc_non_reps.select(['accession', 'checkm2_adjusted_genome_size', 'gtdb_taxonomy']),
-    ])
-
-    gc = gc.with_columns(pl.col("gtdb_taxonomy").str.split(';').list.get(0).alias("kingdom"))
-    gc = gc.with_columns(pl.col("gtdb_taxonomy").str.split(';').list.get(1).alias("phylum"))
-    gc = gc.with_columns(pl.col("gtdb_taxonomy").str.split(';').list.get(2).alias("class"))
-    gc = gc.with_columns(pl.col("gtdb_taxonomy").str.split(';').list.get(3).alias("order"))
-    gc = gc.with_columns(pl.col("gtdb_taxonomy").str.split(';').list.get(4).alias("family"))
-    gc = gc.with_columns(pl.col("gtdb_taxonomy").str.split(';').list.get(5).alias("genus"))
-    gc = gc.with_columns(pl.col("gtdb_taxonomy").str.split(';').list.get(6).alias("species"))
-    logging.info("GTDB taxonomy parsed.")
-
-    levels = ['kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']
-    final = None
-
-    for level in levels:
-        taxon_means = gc.select([level, 'checkm2_adjusted_genome_size']).group_by(level).mean()
-        taxon_means.columns = ['rank', 'genome_size']
-        taxon_mins = gc.select([level, 'checkm2_adjusted_genome_size']).group_by(level).min()
-        taxon_mins.columns = ['rank', 'min_genome_size']
-        taxon_maxs = gc.select([level, 'checkm2_adjusted_genome_size']).group_by(level).max()
-        taxon_maxs.columns = ['rank', 'max_genome_size']
-        taxon_99pct = gc.select([level, 'checkm2_adjusted_genome_size']).group_by(level).quantile(0.99, interpolation='linear')
-        taxon_99pct.columns = ['rank', '99pct_genome_size']
-
-        new_df = taxon_means.join(taxon_mins, on='rank', how="inner").join(taxon_maxs, on='rank', how="inner").join(taxon_99pct, on='rank', how="inner")
-
-        if final is None:
-            final = new_df
-        else:
-            final = pl.concat([
-                final,
-                new_df
-            ])
-    logging.info("Calculated genome size statistics for {} taxa".format(len(final)))
 
     final.write_csv("/dev/stdout", separator="\t")
