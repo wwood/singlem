7ab867c605a18dd27c1fc4359f57f2251682e7f8
Ben Woodcroft <donttrustben@gmail.com>
2024-07-18 11:51:18 +1000

    v0.18.2

--- docker/build.sh
+++ docker/build.sh
@@ -5,6 +5,12 @@ export SINGLEM_VERSION=`../bin/singlem --version`
 export SINGLEM_DOCKER_VERSION=wwood/singlem:$SINGLEM_VERSION
 export SINGLEM_DB_BASENAME=`basename $SINGLEM_METAPACKAGE_PATH`
 
+# Check SINGLEM_DB_BASENAME is not an empty string, else croak
+if [ -z "$SINGLEM_DB_BASENAME" ]; then
+    echo "SINGLEM_METAPACKAGE_PATH must be set to the path of the metapackage to use"
+    exit 1
+fi
+
 cp ../singlem.yml . && \
 sed 's/SINGLEM_VERSION/'$SINGLEM_VERSION'/g; s/SINGLEM_DB_BASENAME/'$SINGLEM_DB_BASENAME'/g' Dockerfile.in > Dockerfile && \
 DOCKER_BUILDKIT=1 docker build -t $SINGLEM_DOCKER_VERSION . && \
