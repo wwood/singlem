cf716831b15312e59485e51cfa5f03b4a7656d34
rzhao-2 <rossen.zhao@gmail.com>
2025-02-03 13:22:10 +1000

    lyrebird: Enable metapackage download and defauts.

--- singlem/lyrebird.py
+++ singlem/lyrebird.py
@@ -25,7 +25,7 @@ import singlem.pipe as pipe
 from singlem.main import add_common_pipe_arguments, add_less_common_pipe_arguments, validate_pipe_args, add_condense_arguments, generate_streaming_otu_table_from_args, get_min_orf_length, get_min_taxon_coverage
 from singlem.pipe import SearchPipe
 from singlem.condense import Condenser
-from singlem.metapackage import DATA_ENVIRONMENT_VARIABLE, CUSTOM_TAXONOMY_DATABASE_NAME
+from singlem.metapackage import LYREBIRD_DATA_ENVIRONMENT_VARIABLE, CUSTOM_TAXONOMY_DATABASE_NAME
 from singlem import OTU_TABLE_OUTPUT_FORMAT, ARCHIVE_TABLE_OUTPUT_FORMAT
 
 from singlem.condense import DEFAULT_MIN_TAXON_COVERAGE as CONDENSE_DEFAULT_MIN_TAXON_COVERAGE
@@ -55,10 +55,10 @@ def main():
         ]},
     )
 
-    data_description = 'Download reference metapackage data'
+    data_description = 'Download Lyrebird reference metapackage data'
     data_parser = bird_argparser.new_subparser('data', data_description, parser_group='Tools')
     # TODO: Could make pipe invocation faster by moving DATA_ENVIRONMENT to a separate file
-    data_parser.add_argument('--output-directory', help="Output directory [required unless {} is specified]".format(DATA_ENVIRONMENT_VARIABLE))
+    data_parser.add_argument('--output-directory', help="Output directory [required unless {} is specified]".format(LYREBIRD_DATA_ENVIRONMENT_VARIABLE))
     data_parser.add_argument('--verify-only', help="Check that the data is up to date and each file has the correct checksum", action='store_true', default=False)
 
     pipe_description = 'Generate a taxonomic profile or OTU table for dsDNA phages from raw sequences'
@@ -140,6 +140,7 @@ def main():
             output_taxonomic_profile = args.taxonomic_profile,
             output_taxonomic_profile_krona = args.taxonomic_profile_krona,
             viral_profile_output = True,
+            parse_lyrebird_metapackage = True,
             exclude_off_target_hits = args.exclude_off_target_hits,
             min_taxon_coverage = get_min_taxon_coverage(args),
             max_species_divergence = args.max_species_divergence,
@@ -188,14 +189,17 @@ def main():
             min_taxon_coverage = args.min_taxon_coverage,
             output_after_em_otu_table = args.output_after_em_otu_table)
 
-    elif args.subparser_name=='data': #TODO: need lyrebird metapackage on zenodo
-        raise NotImplementedError("Lyrebird metapackage not yet available on Zenodo")
+    elif args.subparser_name=='data': #TODO: need lyrebird metapackage on zenodo > done
+        # raise NotImplementedError("Lyrebird metapackage not yet available on Zenodo")
         from singlem.metapackage import Metapackage
         if args.verify_only:
-            Metapackage.verify(output_directory = args.output_directory)
+            Metapackage.verify(output_directory = args.output_directory,
+                                lyrebird = True,
+            )
         else:
             Metapackage.download(
                 output_directory = args.output_directory,
+                lyrebird = True,
             )
 
     else:
