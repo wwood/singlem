33c7e44184b64fcbdf1e1eb026412b2e4e6acddd
Ben Woodcroft <benjwoodcroft@gmail.com>
2025-05-01 10:01:20 +1000

    release: Increments towards.

--- admin/set_env_vars.sh
+++ admin/set_env_vars.sh
@@ -2,15 +2,17 @@
 
 SINGLEM_METAPACKAGE_PATH_RELATIVE="db/S5.4.0.GTDB_r226.metapackage_20250331.smpkg.zb"
 LYREBIRD_METAPACKAGE_PATH_RELATIVE="db/phrog4.1_v0.2.2024_11_7.smpkg.zb"
+GTDBTK_DATA_PATH_RELATIVE="db/release226"
 
-if [[ -e "$SINGLEM_METAPACKAGE_PATH_RELATIVE" ]]; then
-    export SINGLEM_METAPACKAGE_PATH="$(realpath "$SINGLEM_METAPACKAGE_PATH_RELATIVE")"
-else
-    echo "Metapackage $(basename "$SINGLEM_METAPACKAGE_PATH_RELATIVE") not found in db/" >&2
-fi
-
-if [[ -e "$LYREBIRD_METAPACKAGE_PATH_RELATIVE" ]]; then
-    export LYREBIRD_METAPACKAGE_PATH="$(realpath "$LYREBIRD_METAPACKAGE_PATH_RELATIVE")"
-else
-    echo "Metapackage $(basename "$LYREBIRD_METAPACKAGE_PATH_RELATIVE") not found in db/" >&2
-fi
+# For each of the above variables, check if the file exists in the relative path
+# and if it does, set the corresponding environment variable to the absolute path.
+for var in SINGLEM_METAPACKAGE_PATH \
+           LYREBIRD_METAPACKAGE_PATH \
+           GTDBTK_DATA_PATH; do
+    relative_path="${var}_RELATIVE"
+    if [[ -e "${!relative_path}" ]]; then
+        export "$var"="$(realpath "${!relative_path}")"
+    else
+        echo "File $(basename "${!relative_path}") not found in db/" >&2
+    fi
+done
