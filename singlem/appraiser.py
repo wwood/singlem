b3cc5c87d53ec76ee5e66c8bac1ed27644656b8d
AroneyS <samuel.aroney@qut.edu.au>
2025-05-13 07:44:13 +1000

    test 1_000_000 row chunks

--- singlem/appraiser.py
+++ singlem/appraiser.py
@@ -445,7 +445,7 @@ class Appraiser:
         sdb_tmp = sequence_database.acquire(sdb_path)
 
         querier = Querier()
-        for chunk in chunk_collection(metagenome_otu_table_collection, 64_000):
+        for chunk in chunk_collection(metagenome_otu_table_collection, 1_000_000):
             otus_with_hits = []
             otus_without_hits = []
 
