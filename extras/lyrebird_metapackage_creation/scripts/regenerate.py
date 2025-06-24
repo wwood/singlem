0b3fc22ac84870d69881c2f36b2a392995bc09bf
rzhao-2 <rossen.zhao@gmail.com>
2025-06-24 14:34:10 +1000

    lyrebird_metapackage_creation: Regenerate now properly accounts for markers with no off-target seqs.

--- extras/lyrebird_metapackage_creation/scripts/regenerate.py
+++ extras/lyrebird_metapackage_creation/scripts/regenerate.py
@@ -20,9 +20,15 @@ sequence_prefix = snakemake.params.sequence_prefix
 if os.path.exists(output_spkg):
     shutil.rmtree(output_spkg)
 
-try:
-    print(extern.run(f"singlem regenerate --input-singlem-package {spkg} --output-singlem-package {output_spkg} --min-aligned-percent 0 --input-sequences {seqs} --input-taxonomy {taxonomy} --candidate-decoy-sequences {off_target_seqs} --candidate-decoy-taxonomy {off_target_taxonomy} --sequence-prefix {sequence_prefix} &> {snakemake.log}"))
-except Exception as e:
+num_off_target = 0
+with open(off_target_taxonomy, "r") as f:
+    for line in f:
+        num_off_target += 1
+
+if num_off_target == 0:
+    print("Regenerating SingleM package with no off-target sequences")
     print(extern.run(f"singlem regenerate --input-singlem-package {spkg} --output-singlem-package {output_spkg} --min-aligned-percent 0 --input-sequences {seqs} --input-taxonomy {taxonomy} --no-further-euks --sequence-prefix {sequence_prefix} &>> {snakemake.log}"))
+else:
+    print(extern.run(f"singlem regenerate --input-singlem-package {spkg} --output-singlem-package {output_spkg} --min-aligned-percent 0 --input-sequences {seqs} --input-taxonomy {taxonomy} --candidate-decoy-sequences {off_target_seqs} --candidate-decoy-taxonomy {off_target_taxonomy} --sequence-prefix {sequence_prefix} &> {snakemake.log}"))
 
 with open(snakemake.output.done, "w") as _: pass
