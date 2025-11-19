0847db17abbf272b1bff184b0c29438f7d4e8cc3
Ben Woodcroft <donttrustben@gmail.com>
2025-11-19 14:05:29 +1000

    v0.20.3

--- singlem/version.py
+++ singlem/version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.20.2'
-__version_tuple__ = version_tuple = (0, 20, 2)
+__version__ = version = '0.20.3'
+__version_tuple__ = version_tuple = (0, 20, 3)
 
 __commit_id__ = commit_id = None
